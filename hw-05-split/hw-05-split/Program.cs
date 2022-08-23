eb3fa05fafe60b956394b8587c8bc2f973bf3dcb
lattecoff <atkbs@gmail.com>
2022-08-23 20:33:22 +0300

    Added cont input console items.

--- hw-05-split/hw-05-split/Program.cs
+++ hw-05-split/hw-05-split/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+uint cntIntItem = 0;
+uint cntDoubletem = 0;
+uint cntStringItem = 0;
+
+
+foreach (var item in args)
+{
+	//Console.WriteLine(item);
+
+	if (int.TryParse(item, out var valueI))
+	{
+		cntIntItem++;
+		
+	}
+	else if (double.TryParse(item, out var valueD))
+	{
+		cntDoubletem++;
+		
+	}
+	else
+	{
+		cntStringItem++;
+		
+	}
+}
+
+
+Console.WriteLine(cntIntItem);
+Console.WriteLine(cntDoubletem);
+Console.WriteLine(cntStringItem);
